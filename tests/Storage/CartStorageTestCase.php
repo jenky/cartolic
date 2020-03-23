189a3393d49990e34d4b4f83fdf0b9788b4eb8e6
Lynh Tran <jenky.w0w@gmail.com>
2020-03-23 18:12:02 +0700

    Test

--- tests/Storage/CartStorageTestCase.php
+++ tests/Storage/CartStorageTestCase.php
@@ -2,10 +2,12 @@
 
 namespace Jenky\Cartolic\Tests\Storage;
 
+use Brick\Math\RoundingMode;
 use Brick\Money\Money as BrickMoney;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Collection;
 use Jenky\Cartolic\Contracts\Cart\Cart;
+use Jenky\Cartolic\Fee;
 use Jenky\Cartolic\Money;
 use Jenky\Cartolic\Purchasable;
 use Jenky\Cartolic\Tests\TestCase;
@@ -27,7 +29,7 @@ abstract class CartStorageTestCase extends TestCase
             ->withName($this->faker->macProcessor)
             ->withDescription($this->faker->words(10, true))
             ->withPrice(new Money(
-                BrickMoney::ofMinor($this->faker->numberBetween(1000), $this->faker->currencyCode)
+                BrickMoney::ofMinor($this->faker->numberBetween(1000), 'USD') // Todo: use faker->currencyCode
             ));
     }
 
@@ -36,9 +38,9 @@ abstract class CartStorageTestCase extends TestCase
         $items = \Jenky\Cartolic\Facades\Cart::items();
 
         $this->assertInstanceOf(Collection::class, $items);
-        $this->assertTrue($items->isEmpty());
+        $this->assertTrue($items->isEmpty(), 'Cart items are not empty');
         $this->assertEmpty($items->toArray());
-        $this->assertTrue(\Jenky\Cartolic\Facades\Cart::total()->isZero());
+        $this->assertTrue(\Jenky\Cartolic\Facades\Cart::total()->isZero(), 'Cart total is not zero');
     }
 
     public function test_add_item_to_cart()
@@ -53,10 +55,9 @@ abstract class CartStorageTestCase extends TestCase
         $cart->add($item->purchasable(), 5);
         $this->assertCount(1, $cart->items());
         $this->assertEquals(6, $item->quantity());
-        // $this->assertTrue($cart->total()->isPositive());
+        $this->assertTrue($cart->total()->isPositive(), 'Cart total is not zero');
 
-        $cart->clear();
-        $this->assertEmpty($cart->items());
+        $this->assertTrue($item->total()->isEqualTo($cart->subtotal()), 'Items total is equals to cart sub total');
     }
 
     public function test_remove_item()
@@ -68,8 +69,48 @@ abstract class CartStorageTestCase extends TestCase
         $this->assertCount(1, $cart->items());
         $this->assertEquals(9, $item->quantity());
 
+        $cart->remove($item->purchasable(), 4);
+        $this->assertCount(1, $cart->items());
+        $this->assertEquals(5, $item->quantity());
+
         $cart->remove($item->purchasable());
         $this->assertCount(0, $cart->items());
         $this->assertEmpty($cart->items());
+    }
+
+    public function test_clear_cart()
+    {
+        $cart = $this->app->make(Cart::class);
+        $cart->add($this->item, $this->faker->randomNumber(1));
+
+        $cart->clear();
+        $this->assertEmpty($cart->items());
+        $this->assertTrue($cart->total()->isZero(), 'Cart total is not zero');
+        $this->assertTrue($cart->fees()->amounts()->isZero(), 'Cart fees are not zero');
+    }
+
+    public function test_fees()
+    {
+        $cart = $this->app->make(Cart::class);
+        $cart->add($this->item, $this->faker->randomNumber(1));
+
+        $this->assertNotEmpty($cart->items());
+
+        $discount = Fee::make('50% off', $cart->subtotal()->dividedBy(2, RoundingMode::DOWN)->negated());
+        $cart->fees()->add($discount);
+
+        $this->assertCount(1, $cart->fees()->all());
+        $this->assertTrue($cart->total()->isPositive(), 'Cart total is not zero');
+        $this->assertTrue($cart->fees()->amounts()->isNegative(), 'Cart fees are not zero');
+
+        $shipping = Fee::make('shipping', new Money(BrickMoney::ofMinor(199, 'USD')));
+        $cart->fees()->add($shipping);
+
+        $this->assertCount(2, $cart->fees()->all());
+        $this->assertTrue($cart->total()->isPositive(), 'Cart total is not zero');
+
+        $this->assertTrue($cart->total()->isEqualTo(
+            $cart->subtotal()->plus($cart->fees()->amounts())
+        ), 'Cart total is equals to subtotal plus fees');
     }
 }
